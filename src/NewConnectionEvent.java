dfbea507065f6318b048230d6c081e92a51c5002
Alexandra <alexandra.hou@gmail.com>
2016-05-29 12:31:19 +0200

    added functionality in NewConnectionEvent.

--- src/NewConnectionEvent.java
+++ src/NewConnectionEvent.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class NewConnectionEvent extends InfoEvent {
-    public NewConnectionEvent(int id) {
+
+    private String ip;
+    private HashMap<Integer, String> connections;
+
+    public NewConnectionEvent(int id, String ip) {
         super(id);
+        this.ip = ip;
+        connections = new HashMap<>();
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void addConnection(int id, String ip) {
+        connections.put(id, ip);
+    }
+
+    public HashMap<Integer, String> getConnections() {
+        return connections;
     }
 }
